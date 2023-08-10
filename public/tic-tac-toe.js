82079052ce7b6bb72d1eeb253d20a455f5b12164
Nikola Milinovich <nmilinovich1@gmail.com>
2023-08-09 20:40:16 -0700

    phase 2 done

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,1 +1,15 @@
-// Your code here
+let playerTurn = "x"
+window.onload = () => {
+    const cells = document.querySelectorAll(".cell")
+    cells.forEach(cell => {
+        cell.addEventListener("click", () => {
+            if (cell.childNodes.length === 0) {
+                cell.classList.add("occupied");
+                const img = document.createElement("img");
+                img.src = playerTurn === "x" ? "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg" : "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg"
+                playerTurn = playerTurn === "x" ? "o" : "x";
+                cell.appendChild(img);
+            }
+    })
+});
+}
